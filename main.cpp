73c5b205af6746a322cde5076003f7e413acffe3
Fireblaster63200 <happykidies@googlemail.com>
2017-02-14 15:44:41 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include<string.h>
+using namespace std;
+int main()
+{   
+   int i,n,k,count = 0,j;
+   char hack_string[50];
+   char a[50] = "HITMO";
+   char temp;
+   cout<<"\nEnter any random string ";
+   cin>>hack_string;
+    n = strlen(hack_string);
+for(j = 0;j<n;j++)
+{
+   for(i=0;i<n;i++)
+   {
+        temp = hack_string[i];
+        hack_string[i] = hack_string[j];
+        hack_string[j] = temp;
+        cout<<hack_string<<endl;
+        if(strcmp(hack_string,a) == 0)
+            count++;
+   }
+}
+   cout<<"\nNumber of enteries found are "<<count;
+   return 0;
+}
